19a3e7922ebecefb5e19e1aaa5e5958201e0dc74
marioacar <mcarvantes@ucol.mx>
2022-03-27 02:08:42 -0600

    Agregar archivos de proyecto.

---
+++ Contactos/ViewModel/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Contactos.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnpropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion   
+    }
+}
---
+++ Contactos/ViewModel/ContactoViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Contactos.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Contactos.ViewModel
+{
+    public class ContactoViewModel : BaseViewModel
+    {
+        #region Propiedades
+        public ObservableCollection<Contacto> Contactos { get; set; }
+        #endregion
+
+
+        public ContactoViewModel()
+        {
+            Contactos = new ObservableCollection<Contacto>();
+            Contactos.Add(new Contacto()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = "Mario",
+                ApellidoPaterno = "Carvantes",
+                ApellidoMaterno = "Gutierrez",
+                Organizacion = "UdC",
+                Telefonos = new ObservableCollection<Telefono>() { new Telefono{Id = Guid.NewGuid().ToString(), Numero= "246810" },
+                new Telefono{Id = Guid.NewGuid().ToString(), Numero="102030"},
+                }
+            });
+
+            Contactos.Add(new Contacto()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = "Axel",
+                ApellidoPaterno = "Bell",
+                ApellidoMaterno = "Woods",
+                Organizacion = "UdC",
+                Telefonos = new ObservableCollection<Telefono>() { new Telefono{Id = Guid.NewGuid().ToString(), Numero= "123456" },
+                new Telefono{Id = Guid.NewGuid().ToString(), Numero="312312456"},
+                }
+            });
+        }
+    }
+}
